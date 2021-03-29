9783c4fc395640c3cdbe4a1dd3e1f5c153d84ced
Daniel Ssejjemba <ssejjembadan@gmail.com>
2021-03-29 11:20:04 +0300

    Add lesson 06 and exercise with final assesment files for the answers

---
+++ final/04.js
@@ -0,0 +1,41 @@
+// Styling
+
+import * as React from "react";
+import "../box-styles.css";
+
+const smallBox = (
+    <div
+        className="box box--small"
+        style={{ fontStyle: "italic", backgroundColor: "lightblue" }}
+    >
+        small lightblue box
+    </div>
+);
+const mediumBox = (
+    <div
+        className="box box--medium"
+        style={{ fontStyle: "italic", backgroundColor: "pink" }}
+    >
+        medium pink box
+    </div>
+);
+const largeBox = (
+    <div
+        className="box box--large"
+        style={{ fontStyle: "italic", backgroundColor: "orange" }}
+    >
+        large orange box
+    </div>
+);
+
+function App() {
+    return (
+        <div>
+            {smallBox}
+            {mediumBox}
+            {largeBox}
+        </div>
+    );
+}
+
+export default App;
---
+++ final/05.extra-3.js
@@ -0,0 +1,43 @@
+// Controlled Forms
+// 💯 Control the input value
+
+import * as React from "react";
+
+function UsernameForm({ onSubmitUsername }) {
+    const [username, setUsername] = React.useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onSubmitUsername(username);
+    }
+
+    function handleChange(event) {
+        setUsername(event.target.value.toLowerCase());
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="usernameInput">Username:</label>
+                <input
+                    id="usernameInput"
+                    type="text"
+                    onChange={handleChange}
+                    value={username}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
+
+function App() {
+    const onSubmitUsername = (username) => alert(`You entered: ${username}`);
+    return (
+        <div style={{ minWidth: 400 }}>
+            <UsernameForm onSubmitUsername={onSubmitUsername} />
+        </div>
+    );
+}
+
+export default App;
